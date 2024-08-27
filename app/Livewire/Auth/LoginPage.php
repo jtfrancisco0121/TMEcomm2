61fcf21b7601df886a5df06bd7fc58ca7e196142
jtfrancisco0121 <jtfrancisco21@gmail.com>
2024-08-27 10:26:50 +0800

    Move to Windows OS

---
+++ app/Livewire/Auth/LoginPage.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+#[Title('Login - CareSource')]
+class LoginPage extends Component
+{
+    public $email;
+
+    public $password;
+
+    public function save()
+    {
+        $this->validate([
+            'email' => 'required|email|exists:users,email|max:255',
+            'password' => 'required|min:6|max:255',
+        ]);
+
+        if (! auth()->attempt(['email' => $this->email, 'password' => $this->password])) {
+            session()->flash('error', 'Invalid Credentials');
+
+            return;
+        }
+
+        return redirect()->intended();
+    }
+
+    public function render()
+    {
+        return view('livewire.auth.login-page');
+    }
+}
